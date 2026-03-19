4d14bc8f502e2eddef9ccb8df99ada85d92311f1
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-03-18 22:47:04 -0400

    added const and simplified if

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,14 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const total = days * 40;
+  const COST_PER_DAY = 40;
+  const total = days * COST_PER_DAY;
 
   if (days >= 7) {
     return total - 50;
-  } else if (days >= 3) {
+  } 
+  if (days >= 3) {
     return total - 20;
   }
-
   return total;
 }
 
