19bc183e70eeffeafc225dac479fde2dab456060
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-03-18 22:26:20 -0400

    tast1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const total = days * 40;
+
+  if (days >= 7) {
+    return total - 50;
+  } else if (days >= 3) {
+    return total - 20;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
