57a82032d1e5d79f07cee569cc073a437594d848
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-03-18 22:52:36 -0400

    added constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,14 +5,18 @@
  */
 function calculateRentalCost(days) {
   const COST_PER_DAY = 40;
+  const LONG_RENTAL_DISCOUNT = 50;
+  const BASIC_DISCOUNT = 20;
+  const LONG_RENTAL_DISCOUNT_THRESHOLD = 7;
+  const BASIC_DISCOUNT_THRESHOLD = 3;
   const total = days * COST_PER_DAY;
 
-  if (days >= 7) {
-    return total - 50;
+  if (days >= LONG_RENTAL_DISCOUNT_THRESHOLD) {
+    return total - LONG_RENTAL_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return total - 20;
+  if (days >= BASIC_DISCOUNT_THRESHOLD) {
+    return total - BASIC_DISCOUNT;
   }
 
   return total;
